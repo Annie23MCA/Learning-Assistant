a8d81323309e24d6d04b2d81ec9c21d77b5a093d
Annie23MCA <157511320+Annie23MCA@users.noreply.github.com>
2025-04-14 17:23:33 +0530

    Add files via upload

---
+++ objective.py
@@ -0,0 +1,109 @@
+import re
+import nltk
+import numpy as np
+from nltk.corpus import wordnet as wn
+
+class ObjectiveTest:
+
+    def __init__(self, data, noOfQues):
+        self.summary = data
+        self.noOfQues = noOfQues
+
+    def get_trivial_sentences(self):
+        sentences = nltk.sent_tokenize(self.summary)
+        trivial_sentences = []
+        for sent in sentences:
+            trivial = self.identify_trivial_sentences(sent)
+            if trivial:
+                trivial_sentences.append(trivial)
+        return trivial_sentences
+
+    def identify_trivial_sentences(self, sentence):
+        tokens = nltk.word_tokenize(sentence)
+        tags = nltk.pos_tag(tokens)
+        if tags[0][1] == "RB" or len(tokens) < 4:
+            return None
+
+        noun_phrases = []
+        grammar = r"""
+            CHUNK: {<NN>+<IN|DT>*<NN>+}
+                   {<NN>+<IN|DT>*<NNP>+}
+                   {<NNP>+<NNS>*}
+        """
+        chunker = nltk.RegexpParser(grammar)
+        pos_tokens = nltk.pos_tag(tokens)
+        tree = chunker.parse(pos_tokens)
+
+        for subtree in tree.subtrees():
+            if subtree.label() == "CHUNK":
+                temp = " ".join([sub[0] for sub in subtree])
+                noun_phrases.append(temp.strip())
+
+        replace_nouns = []
+        for word, _ in tags:
+            for phrase in noun_phrases:
+                if phrase[0] == '\'':
+                    break
+                if word in phrase:
+                    replace_nouns.extend(phrase.split()[-2:])
+                    break
+            if not replace_nouns:
+                replace_nouns.append(word)
+            break
+
+        if not replace_nouns:
+            return None
+
+        val = min(len(i) for i in replace_nouns)
+
+        trivial = {
+            "Answer": " ".join(replace_nouns),
+            "Key": val
+        }
+
+        if len(replace_nouns) == 1:
+            trivial["Similar"] = self.answer_options(replace_nouns[0])
+        else:
+            trivial["Similar"] = []
+
+        replace_phrase = " ".join(replace_nouns)
+        blanks_phrase = ("__________ " * len(replace_nouns)).strip()
+        expression = re.compile(re.escape(replace_phrase), re.IGNORECASE)
+        sentence = expression.sub(blanks_phrase, sentence, count=1)
+        trivial["Question"] = sentence
+        return trivial
+
+    @staticmethod
+    def answer_options(word):
+        synsets = wn.synsets(word, pos="n")
+
+        if not synsets:
+            return []
+        else:
+            synset = synsets[0]
+
+        if synset.hypernyms():
+            hypernym = synset.hypernyms()[0]
+            hyponyms = hypernym.hyponyms()
+            similar_words = []
+            for hyponym in hyponyms:
+                similar_word = hyponym.lemmas()[0].name().replace("_", " ")
+                if similar_word != word:
+                    similar_words.append(similar_word)
+                if len(similar_words) == 8:
+                    break
+            return similar_words
+        else:
+            return []
+
+    def generate_test(self):
+        trivial_pair = self.get_trivial_sentences()
+        question_answer = [qa for qa in trivial_pair if qa["Key"] > int(self.noOfQues)]
+        question = []
+        answer = []
+        while len(question) < int(self.noOfQues):
+            rand_num = np.random.randint(0, len(question_answer))
+            if question_answer[rand_num]["Question"] not in question:
+                question.append(question_answer[rand_num]["Question"])
+                answer.append(question_answer[rand_num]["Answer"])
+        return question, answer
